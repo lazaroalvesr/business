56a3694be0f1a0cf9d0142944b8dcf1ddd5b1038
lazaro <lazaroalves12355@gmail.com>
2023-11-24 09:15:27 -0300

    de

--- src/app/components/Solutions.tsx
+++ src/app/components/Solutions.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Solutions() {
   return (
     <div className=" ">
-      <div className="bg-blue-900 w-full">
+      <div className="bg-blue-900 w-full min-h-screen">
         <h1 className="pb-12">pao</h1>
         {/* <div className="grid lg:grid-cols-2 grid-cols-1 ml-3" >
           <div className="mt-20 ">
